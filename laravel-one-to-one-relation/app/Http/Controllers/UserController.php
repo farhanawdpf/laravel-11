f0ff5b16ecd3577defe6fb2ae87e38aea2b173b0
farhanawdpf <farhanawdpf@gmail.com>
2024-08-22 19:06:35 +0600

    update

---
+++ laravel-one-to-one-relation/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Phone;
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        // $users = Phone::find(1);
+        $users = Phone::find(1)->user;
+        $users = User::all();
+        // return $users;
+        return view('user', compact('users'));
+    }
+}
